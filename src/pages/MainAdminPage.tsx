bf53be9f8ba280e945d0a0a69c3cfaee5164ff4b
muro472 <keklol472@gmail.com>
2023-11-15 05:51:41 +0200

    now slee[

--- src/pages/MainAdminPage.tsx
+++ src/pages/MainAdminPage.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router-dom";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { RouterLinks } from "../types/routerLinks.ts";
 
 import "../scss/pages/_main-admin-page.scss";
+import { useEffect } from "react";
 
 const isActiveNavLink = ({ isActive }: { isActive: boolean }) =>
   isActive ? "main-admin-page__nav-linkActive" : "main-admin-page__nav-link";
 
 export default function MainAdminPage() {
+  let location = useLocation();
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate(RouterLinks.ITEMS_PATH);
+    }
+  }, [location]);
+
   return (
     <>
       <nav className="main-admin-page__nav">
